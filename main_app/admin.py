ab72437793c1c4b5e8d99bcfbd2ffd4ff5d0a477
Emily Johnson <emilyjohnson@Emilys-iMac.attlocal.net>
2021-10-13 17:16:52 -0700

    test

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Breweries, Beer, Favorite
 # Register your models here.
+admin.site.register(Breweries)
+admin.site.register(Beer)
+admin.site.register(Favorite)
+
+
